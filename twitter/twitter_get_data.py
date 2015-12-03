fb5c24b6a895a52508839fa760ed89269498307f
SnowMasaya <snow.akogi.pgel@gmail.com>
2015-12-03 13:09:40 +0900

    .gitignore is now working

--- twitter/twitter_get_data.py
+++ twitter/twitter_get_data.py
@@ -8,13 +8,15 @@ twitter_auth = twitter_key()
 t = Twitter(auth=OAuth(twitter_auth.token, twitter_auth.token_key, twitter_auth.con_secret, twitter_auth.con_secret_key))
 
 modelSearchList = ModelSearchList()
+f = open("twitter_label_count.txt", "w")
 
 for item in modelSearchList.search_list:
 #for item in modelSearchList.search_list_test:
-    data = t.search.tweets(q=item, count=10)
-    print(len(data["statuses"]))
+    data = t.search.tweets(q=item, count=400)
+    f.write(str(len(data["statuses"])) +","+ item + "\n")
     for i in range(len(data["statuses"])):
         out_data = data["statuses"][i]["text"].replace("\n", "").replace(",", "") 
         print(out_data, end='')
         print("," + item) 
         #print("," + data["statuses"][i]["entities"]["hashtags"][0]["text"])
+f.close()
--- watson_api/extract_test_text.py
+++ watson_api/extract_test_text.py
@@ -1,44 +1,50 @@
 import json
 from os.path import join, dirname
-from watson_developer_cloud import NaturalLanguageClassifierV1 as NaturalLanguageClassifier
-from watson_key import watson_key
 from argparse import ArgumentParser
 import subprocess
 import sys
 model_path = (subprocess.check_output("echo $Watson_mdel", stderr=subprocess.STDOUT, shell=True)).decode('utf-8').strip() 
 sys.path.append(model_path)
 from model_search_list import ModelSearchList
-from sklearn.metrics import f1_score
 
 
-class Watson_api():
+class ExtractTextApi():
 
     def __init__(self):
-        self.fname = "" 
         self.modelSearchList = ModelSearchList()
         self.text_data = []
         self.target_label = []
-        self.watson_crediantial = watson_key()
-        self.watson_classifier = self.watson_crediantial.classifier_twitter_hash_classfier
-        self.natural_language_classifier = NaturalLanguageClassifier(username=self.watson_crediantial.username,
-                                                                     password=self.watson_crediantial.password)
-        #print(json.dumps(self.natural_language_classifier.list(), indent=2))
+        self.predict_data = []
 
     def parse_args(self):
 
         p = ArgumentParser(description='Encoder-decoder neural machine trainslation')
-        p.add_argument('data', help='[in] data')
+        p.add_argument('target_data', help='[in] target_data')
+        p.add_argument('predict_data', help='[in] predict_data')
+        p.add_argument('text_data', help='[in] text_data')
         args = p.parse_args()
 
         return args
 
-    def __read_data(self):
-        for line in open(self.fname, "r"):
-            split_line = line.split(",")
-            self.text_data.append(split_line[0].strip())
-            self.target_label.append(self.modelSearchList.search_dictionary[split_line[1].strip()])
+    def extract_missing_data(self, args):
+        for line in open(args.target_data, "r"):
+            self.target_data = line.strip().replace("[", "").replace("]", "").split(",")
+            self.target_data = [int(x) for x in self.target_data] 
+        for line in open(args.predict_data, "r"):
+            self.predict_data = line.strip().replace("[", "").replace("]", "").split(",")
+            self.predict_data = [int(x) for x in self.predict_data] 
+        for line in open(args.text_data, "r"):
+            text_data = line.strip().split(",")
+            self.text_data.append(text_data[0])
+
+        print("target label,predict label,text")
+        for i in range(len(self.predict_data)): 
+            if self.target_data[i] != self.predict_data[i]:
+                print(self.modelSearchList.search_char_dictionary[self.target_data[i]]+ ",", end="")
+                print(self.modelSearchList.search_char_dictionary[self.predict_data[i]]+ ",", end ="")
+                print(self.text_data[i])
 
 if __name__ == "__main__":
-    watson_api = Watson_api()
-    args = watson_api.parse_args()
-    watson_api.predict(args)
+    extract_text_api = ExtractTextApi()
+    args = extract_text_api.parse_args()
+    extract_text_api.extract_missing_data(args)
